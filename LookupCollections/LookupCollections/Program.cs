0e3b597c6448947eb04bbf46afb7c5f23964fb16
qayol1 <qayol@freemail.hu>
2017-06-21 17:15:13 +0200

    All collections implemented

---
+++ LookupCollections/LookupCollections/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace LookupCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Make the dictionary case insensitive 
+                ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+
+                // Add some items
+                list["Estados Unidos"] = "United States of America";
+                list["Canadá"] = "Canada";
+                list["España"] = "Spain";
+
+                // Show the results 
+                Console.WriteLine(list["españa"]);
+                Console.WriteLine(list["CANADÁ"]);
+                Console.Read();
+            }
+        }
+    }
+}
